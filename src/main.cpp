e284ec17e39302676343bc8ca18ae81a965be2a2
Xwilarg <xwilarg@yahoo.fr>
2019-04-02 11:31:12 +0200

    Add answer on GET request

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include <cpprest/http_listener.h>
 #include <iostream>
 
+void getRequest(web::http::http_request message)
+{
+	message.reply(web::http::status_codes::OK, web::json::value::string(U("OK")));
+}
+
 int main()
 {
-	std::cout << "Test" << std::endl;
+	web::http::experimental::listener::http_listener listener(L"http://localhost:8082");
+	listener.support(web::http::methods::GET, std::bind(&getRequest, std::placeholders::_1));
+	listener.open().wait();
+	std::cout << "Listening..." << std::endl << "Press enter to exit" << std::endl;
+	std::cin.ignore();
+	listener.close().wait();
 }
